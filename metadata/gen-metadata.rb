49680d6ec96361b6ea4ad6e2cbfa8e3dae787724
John Cupitt <jcupitt@gmail.com>
2019-02-25 17:01:24 +0000

    make combined.tsv as well

--- metadata/gen-metadata.rb
+++ metadata/gen-metadata.rb
@@ -33,37 +33,55 @@ def load_json(json_dir, subject_id)
   json
 end
 
-part_name = "#{out_dir}/participants.tsv"
-CSV.open(part_name, "w", col_sep: "\t") do |participants_tsv|
-  participants_tsv << ["participant_id", "gender", "birth_ga"]
+combined_name = "#{out_dir}/combined.tsv"
+CSV.open(combined_name, "w", col_sep: "\t") do |combined_tsv|
+  combined_tsv << [
+    "participant_id", 
+    "session_id", 
+    "gender", 
+    "birth_ga", 
+    "age_at_scan"
+  ]
 
-  Dir.glob("#{json_dir}/CC*.json") do |subject_dir|
-    next if subject_dir !~ /.*\/(CC.*)\.json/
-    subject_id = $~[1]
+  part_name = "#{out_dir}/participants.tsv"
+  CSV.open(part_name, "w", col_sep: "\t") do |participants_tsv|
+    participants_tsv << ["participant_id", "gender", "birth_ga"]
 
-    json = load_json json_dir, subject_id
-    next if json == nil
+    Dir.glob("#{json_dir}/CC*.json") do |subject_dir|
+      next if subject_dir !~ /.*\/(CC.*)\.json/
+      subject_id = $~[1]
 
-    gender = nil
-    age_at_birth = nil
-    sess_name = "#{out_dir}/sub-#{subject_id}_sessions.tsv"
-    CSV.open(sess_name, "w", col_sep: "\t") do |sessions_tsv|
-      sessions_tsv << ["session_id", "age_at_scan"]
-      json.each do |session|
-        session_id = session["scan_validation"]
-        age_at_scan = session["scan_ga_at_scan_weeks"]
-        age_at_birth = session["baby_ga_at_birth_weeks"]
-        gender = session["baby_gender"] == "F" ? "Female" : "Male"
+      json = load_json json_dir, subject_id
+      next if json == nil
 
-        if session_id !~ /\d+/ || session_id.to_i == 0
-          err "#{subject_id}-#{session_id} bad session_id"
-          next
+      gender = nil
+      age_at_birth = nil
+      sess_name = "#{out_dir}/sub-#{subject_id}_sessions.tsv"
+      CSV.open(sess_name, "w", col_sep: "\t") do |sessions_tsv|
+        sessions_tsv << ["session_id", "age_at_scan"]
+        json.each do |session|
+          session_id = session["scan_validation"]
+          age_at_scan = session["scan_ga_at_scan_weeks"]
+          age_at_birth = session["baby_ga_at_birth_weeks"]
+          gender = session["baby_gender"] == "F" ? "Female" : "Male"
+
+          if session_id !~ /\d+/ || session_id.to_i == 0
+            err "#{subject_id}-#{session_id} bad session_id"
+            next
+          end
+
+          sessions_tsv << [session_id, age_at_scan]
+          combined_tsv << [
+            subject_id, 
+            session_id, 
+            gender, 
+            age_at_birth, 
+            age_at_scan
+          ]
         end
-
-        sessions_tsv << [session_id, age_at_scan]
       end
-    end
 
-    participants_tsv << [subject_id, gender, age_at_birth]
+      participants_tsv << [subject_id, gender, age_at_birth]
+    end
   end
 end
